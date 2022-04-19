5e2f24f5db4100abd95ed8ada497476d2cb38ef8
Gabriel Mataleo <mataleo.gml@gmail.com>
2022-04-19 10:12:20 -0400

    Create gablam20-MATH204-N01-Week1-Exercise1.py

---
+++ Week 1 - Python Basics/gablam20-MATH204-N01-Week1-Exercise1.py
@@ -0,0 +1,15 @@
+#   SCAD Spring 2022 - Math204-N01 - Professor Juras
+#   Week 1 - Python Basics: Problem 1
+
+#   Assume s is a string of lower case characters.
+#   Write a program that counts up the number of vowels contained in the string s. Valid vowels are: 'a', 'e', 'i', 'o', and 'u'.
+#   For example, if s = 'azcbobobegghakl', your program should print:
+#   Number of vowels: 5
+
+count = 0
+s = input("Enter a string: ")
+for n in s:
+    if n == 'a' or n == 'e' or n == 'i' or n == 'o' or n == 'u':
+        count += 1
+        print(n)
+print("Number of vowels:" + str(count))
