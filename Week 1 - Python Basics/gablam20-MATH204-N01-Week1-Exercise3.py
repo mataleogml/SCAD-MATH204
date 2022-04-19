0b4b687debbe8555e96e4a75c988d2328d295be2
Gabriel Mataleo <mataleo.gml@gmail.com>
2022-04-19 10:19:26 -0400

    Update gablam20-MATH204-N01-Week1-Exercise3.py

--- Week 1 - Python Basics/gablam20-MATH204-N01-Week1-Exercise3.py
+++ Week 1 - Python Basics/gablam20-MATH204-N01-Week1-Exercise3.py
@@ -1,18 +1,18 @@
-#   SCAD Spring 2022 - Math204-N01 - Professor Juras
-#   Week 1 - Python Basics: Exercise 3
+# SCAD Spring 2022 - Math204-N01 - Professor Juras
+# Week 1 - Python Basics: Exercise 3
 
-#   Assume s is a string of lower case characters.
+# Assume s is a string of lower case characters.
 
-#   Write a program that prints the longest substring of s in which the letters occur in alphabetical order. For example, if
-#   s = 'azcbobobegghakl', then your program should print:
-#   Longest substring in alphabetical order is: beggh
+# Write a program that prints the longest substring of s in which the letters occur in alphabetical order. For example, if
+# s = 'azcbobobegghakl', then your program should print:
+# Longest substring in alphabetical order is: beggh
 
-#   In the case of ties, print the first substring. For example, if s = 'abcbcd', then your program should print:
-#   Longest substring in alphabetical order is: abc
+# In the case of ties, print the first substring. For example, if s = 'abcbcd', then your program should print:
+# Longest substring in alphabetical order is: abc
 
-#   Note: This problem may be challenging. We encourage you to work smart. If you've spent more than a few hours on this problem, we
-#   suggest that you move on to a different part of the course. If you have time, come back to this problem after you've had a break and
-#   cleared your head.
+# Note: This problem may be challenging. We encourage you to work smart. If you've spent more than a few hours on this problem, we
+# suggest that you move on to a different part of the course. If you have time, come back to this problem after you've had a break and
+# cleared your head.
 
 s = input("Enter a string: ")
 longest_sub = ''
